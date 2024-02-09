e4e2ee86955a8a137a48688a1391ea494ce9f811
TalhaAlam1 <112478647+TalhaAlam1@users.noreply.github.com>
2024-02-09 07:41:47 -0600

    Add files via upload

---
+++ index.java
@@ -0,0 +1,14 @@
+class Test{
+    public static void main(string[] args){
+        system.out.println("1");
+        system.out.println("2");
+        system.out.println("3");
+        system.out.println("4");
+        system.out.println("5");
+        system.out.println("6");
+        system.out.println("7");
+        system.out.println("8");
+        system.out.println("9");
+        system.out.println("10");
+    }
+}
